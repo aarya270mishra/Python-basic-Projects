d6a1afabac1da664de14f5bbdbeaab64ac2c49ad
Aarya Mishra <aaryamishra512@gmail.com>
2026-06-15 20:05:51 +0530

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,7 @@
+import time 
+
 print("Hiii, Welcome")
-ans = input("Do You want to play a quiz game: ")
+ans = str(input(r"Do You want to play a quiz game (yes/no): "))
 
 if ans!="yes":
     quit(0)
@@ -7,9 +9,10 @@ else:
     print("Hurayy!..Lets play") 
 
 print('=================================================================')
+time.sleep(3)
 
 score=0
-que1 = input("The  brain of any computer system is").upper()
+que1 = input("The  brain of any computer system is: ").upper()
 if que1 =="CPU":
     print("Correct !")
     score+=1
@@ -18,7 +21,9 @@ else:
     print("Wrong anser")  
 print(f"Your score {score}")    
 
-ques2 = input("Which of the following language does the computer understand?").lower()
+
+time.sleep(2)
+ques2 = input("Which of the following language does the computer understand? :").lower()
 if ques2 == "binary":
     print("Corect !")
     score+=1
@@ -27,7 +32,7 @@ else:
     print("Wrong answer")    
 print(f"Your score {score}") 
 
-
+time.sleep(2)
 ques3 = input("Which device is used to input data into a computer?").lower()
 if ques3 == "keyboard":
     print("Correct !")
@@ -37,8 +42,8 @@ else:
 print(f"Your score {score}") 
 
 
-
-ques4 = input("What is the full form of RAM?").lower()
+time.sleep(2)
+ques4 = input("What is the full form of RAM? :").lower()
 if ques4 == "random access memory":
     print("Correct !")
     score+=1
@@ -48,12 +53,17 @@ print(f"Your score {score}")
 
 print("Now lets move to the last question")
 
-ques5 = input("What does GUI stand for?").lower()
+
+
+time.sleep(2)
+ques5 = input("What does GUI stand for? :").lower()
 if ques5 == "graphical user interface":
     print("Correct !")
     score+=1
+    time.sleep(2)
 else:
     print("Wrong answer")
+    time.sleep(2)
 print(f"Your Final score is {score} out of 5")     
 
 print("WITH THAT THE QUIZ ENDS") 
--- number_guessing.py
+++ number_guessing.py
@@ -1,13 +1,16 @@
 import random 
+import time 
 
 number=random.randrange(1,50)
 
 print("Lets play guessing the number challaenge")
+time.sleep(2)
 
 print("""=========================THE RULES===================================
       choose any number btw 1-50 
       if thats number closer to the answer then you are hot and if 
       thats far you are cold """)
+time.sleep(2)
 
 while True:
     guess=int(input("Enter your number:"))
